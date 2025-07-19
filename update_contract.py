20a10106f03785a63472e10e8aacfbaa102c1758
jayesh <125264567+jayesh-87@users.noreply.github.com>
2025-07-19 19:23:03 +0530

    Update update_contract.py

--- update_contract.py
+++ update_contract.py
@@ -3,12 +3,12 @@ from web3 import Web3
 import json
 
 # 1. Call the Flask API
-response = requests.get("http://127.0.0.1:5000/recommendation")
+response = requests.get("Your url with port/recommendation")
 recommendation = response.json()["recommendation"]
 print(f"Model recommends: {recommendation}")
 
-# 2. Connect to Ganache
-ganache_url = "http://127.0.0.1:7545"
+# 2. Connect to Ganacheganache_url = os.getenv("GANACHE_URL")
+ganache_url = os.getenv("GANACHE_URL")
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 assert web3.is_connected(), "Failed to connect to Ganache"
 
@@ -16,11 +16,11 @@ assert web3.is_connected(), "Failed to connect to Ganache"
 with open("StockAdvisorABI.json") as f:
     abi = json.load(f)
 
-contract_address = Web3.to_checksum_address("0xedb1d96ca1397c98502bc1b74870a9998f65918c")
+contract_address = Web3.to_checksum_address(os.getenv("CONTRACT_ADDRESS"))
 contract = web3.eth.contract(address=contract_address, abi=abi)
 
 # 4. Send transaction
-account = web3.eth.accounts[0]
+account = web3.eth.accounts[account_index]
 tx_hash = contract.functions.updateRecommendation(recommendation).transact({"from": account})
 web3.eth.wait_for_transaction_receipt(tx_hash)
 print("Smart contract updated successfully!")
